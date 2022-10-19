43a60b0f7448b2222e67c092afa16724d579ab91
stanley <tochukwuu14@gmail.com>
2022-10-19 23:28:43 +0200

    delete and modified some files

--- src/modules/contentAPI.js
+++
@@ -1,5 +0,0 @@
-const apiUrl = 'https://api.tvmaze.com/';
-
-const main = async () => fetch(`${apiUrl}shows`).then((result) => result.json()).then((data) => data.slice(0, 30));
-
-export default main;
