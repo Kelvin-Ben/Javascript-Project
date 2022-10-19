6a3c70b828fe38b948e67e17c9dd43afa451668b
stanley <tochukwuu14@gmail.com>
2022-10-19 18:56:19 +0200

    Made some changes to the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,35 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+
   entry: './src/index.js',
+   
   devServer: {
     static: './dist',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/',
-  },
+
+   plugins: [
+      new HtmlWebpackPlugin({
+      template: './src/index.html'
+      }),
+   ],
+   
+   output: {
+     filename: 'bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+   },
+   
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
-  optimization: {
-    runtimeChunk: 'single',
-  },
-  mode: 'development'
-};
+ };
