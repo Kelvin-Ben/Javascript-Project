0a5246df3a213afef3564392e17634f31cd24243
Kelvin Ben <kevoben14@gmail.com>
2022-10-19 22:37:43 +0300

    Display comment popup for selected items

--- src/index.js
+++ src/index.js
@@ -3,3 +3,29 @@ import img from './assets/logo.png';
 
 const image = document.querySelector('.logo');
 image.innerHTML = `<img class="img-logo" src="${img}" alt="my logo">`;
+
+const modal = document.querySelectorAll('.grid-item');
+const displayModal = document.querySelector('.modal-content');
+const gridContainer = document.querySelector('.grid-container');
+const header = document.querySelector('.header');
+const footer = document.querySelector('.footer');
+
+modal.forEach((mode) => {
+  mode.addEventListener('click', (e) => {
+    e.preventDefault();
+    displayModal.style.display = 'block';
+    gridContainer.style.display = 'none';
+    header.style.display = 'none';
+    footer.style.display = 'none';
+  });
+});
+
+const spanClose = document.querySelector('.close');
+
+spanClose.addEventListener('click', (e) => {
+  e.preventDefault();
+  displayModal.style.display = 'none';
+  gridContainer.style.display = 'grid';
+  header.style.display = 'flex';
+  footer.style.display = 'block';
+});
